cdf073ddfb02378f569e3d2fcda228b9f9cb5816
saima <sayedssaima04@gmail.com>
2021-08-25 15:06:30 +0530

    modified validation messages

--- models/taskSchema.js
+++ models/taskSchema.js
@@ -20,7 +20,7 @@ const taskSchema = mongoose.Schema({
             validator: function(v){
                 return /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])\s(3[01]|[12][0-9]|0[1-9])\/(1[0-2]|0[1-9])\/[0-9]{4}/.test(v);
             },
-            message: props => `Please use the correct format for ${props.value}`,
+            message: props => `Please use the correct format "H:M:S DD/MM/YYYY" for ${props.value}`,
         },
         required: true,
     },
@@ -32,7 +32,7 @@ const taskSchema = mongoose.Schema({
                     return /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])\s(3[01]|[12][0-9]|0[1-9])\/(1[0-2]|0[1-9])\/[0-9]{4}/.test(v);
                 }
             },
-            message: props => `Please use the correct format for ${props.value}`,
+            message: props => `Please use the correct format "H:M:S DD/MM/YYYY" for ${props.value}`,
         },
         default: "",
     }
